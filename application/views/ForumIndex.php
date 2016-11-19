fccd6caafd643c29c0b11ba43e449efea8d31c25
elevanita <anggarielevanita@gmail.com>
2016-11-19 10:46:41 +0700

    pert3

---
+++ application/views/ForumIndex.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ini Page Forum</title>
+</head>
+<body>
+
+<p>Hello...</p>
+
+</body>
+</html>
---
+++ application/views/ForumMember.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ini Page Member</title>
+</head>
+<body>
+
+<p>Hello...</p>
+
+</body>
+</html>
